90b0af34b517ff1314aa2d35b344239e359b687f
imranlatur24 <imranlatur24@gmail.com>
2025-08-05 14:24:33 +0530

    update into models

--- DockerPro/studentapp/models.py
+++ DockerPro/studentapp/models.py
@@ -6,8 +6,10 @@ class StudentModel(models.Model):
     name=models.CharField(max_length=15)
     email=models.EmailField()
     password=models.CharField(max_length=25)
-    create_at=models.DateTimeField(default=timezone.now)
-    updated_at=models.DateTimeField(default=timezone.now)
+    # create_at=models.DateTimeField(default=timezone.now)
+    # updated_at=models.DateTimeField(default=timezone.now)
+    create_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
